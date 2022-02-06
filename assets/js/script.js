116f6125a9debb0432c7c22bbf9c0bca8f57cd59
kalho <luisenriquez11@hotmail.com>
2022-02-06 17:55:33 -0500

    mapquestapi for geocoding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,29 @@
-// https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=7967fdacde126dedab49b6e1e52ecc3f
-let cities = [
-    {
-        "name": "Austin",
-        "url": "https://api.openweathermap.org/data/2.5/onecall?lat=25.76&lon=-80.19&units=imperial&appid=7967fdacde126dedab49b6e1e52ecc3f"
-    },
-    {
-        "name": "Chicago",
-        "url": "" 
-    },
-    {
-        "name": "New York",
-        "url": "" 
-    },{
-        "name": "Orlando",
-        "url": "" 
-    },{
-        "name": "San Francisco",
-        "url": "" 
-    },{
-        "name": "Seatle",
-        "url": "" 
-    },{
-        "name": "Denver",
-        "url": "" 
-    },{
-        "name": "Atlanta",
-        "url": "" 
-    }
-] 
-var cityurl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lan + "&lon=" + lon + "&units=imperial&appid=7967fdacde126dedab49b6e1e52ecc3f"
+
+let cities = []; 
+var cityid = 0;
+// var cityurl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cities.lat + "&lon=" + cities.lon + "&units=imperial&appid=7967fdacde126dedab49b6e1e52ecc3f"
 
 function lookforcity() {
-
+    cityid++;
+    var cityname = document.getElementById("searcharea").value;
+    var citycoords = "http://www.mapquestapi.com/geocoding/v1/address?key=YYCexb1D4aTNStxA70Weai9GVqLtrZ9Q&location=" + cityname;
+    console.log(citycoords);
+    fetch(citycoords).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+            var lat = data.results[0].locations[0].latLng.lat;
+            var lon = data.results[0].locations[0].latLng.lng;
+            let city = {
+                "name": cityname,
+                "lat": lat,
+                "lon": lon
+            }
+            cities.push(city);
+            console.log(cities);
+            localStorage.setItem(cityid, JSON.stringify(city));
+        })
+    })
 }
 
-document.getElementById("searchb").addEventListener("click")
+document.getElementById("searchb").addEventListener("click", lookforcity);
 
-fetch(austin).then(function(response) {
-    response.json().then(function(data) {
-        console.log(data);
-    })
-})
